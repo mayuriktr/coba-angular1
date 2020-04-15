7f08f4e5a7b2c8560753544b73611e3c4333eedf
mayuriktr <c14180078@john.petra.ac.id>
2020-04-15 08:24:57 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ysj3rg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ysj3rg)
---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'KR';
+  itembuah = [{
+    nama :'Kelengkeng',
+    harga : 10000
+  },{
+nama : 'jeruk',
+harga :10000
+
+ }];
+
+}
